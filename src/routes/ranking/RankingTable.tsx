6330321e90edb9c662109f17cfcb2f34a809729d
chichurita <ra.singh069@gmail.com>
2022-07-04 11:31:06 +0200

    testing ci

--- src/routes/ranking/RankingTable.tsx
+++ src/routes/ranking/RankingTable.tsx
@@ -6,6 +6,7 @@ const RankingTable = () => {
             <RankingElement />
             <RankingElement />
             <RankingElement />
+            <RankingElement />
         </div>
     );
 };
