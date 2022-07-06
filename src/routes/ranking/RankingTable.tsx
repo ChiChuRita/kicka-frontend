1b348ea07a4e7379395810202df5f7cd053ea636
chichurita <ra.singh069@gmail.com>
2022-07-06 14:57:54 +0200

    added api domain

--- src/routes/ranking/RankingTable.tsx
+++ src/routes/ranking/RankingTable.tsx
@@ -7,6 +7,7 @@ const RankingTable = () => {
             <RankingElement />
             <RankingElement />
             <RankingElement />
+            <RankingElement />
         </div>
     );
 };
