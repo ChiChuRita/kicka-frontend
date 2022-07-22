cdd722fa513a1e46c5e6b9b345fd2b08ff81b603
chichurita <ra.singh069@gmail.com>
2022-07-22 18:45:46 +0200

    minor changes

--- src/routes/auth/LoginPage.tsx
+++ src/routes/auth/LoginPage.tsx
@@ -2,7 +2,8 @@ import HPILogo from "../../assets/hpi_logo.svg";
 
 const LoginPage = () => {
     return (
-        <div>
+        <div className="flex flex-col grow gap-5">
+            <span>Please Login to compete against the greatest!</span>
             <a
                 className="button flex flex-row items-center gap-4 p-4"
                 href={import.meta.env.VITE_API_URL + "/public/login"}
--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -1,20 +1,37 @@
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQueries } from "react-query";
 import { Link } from "react-router-dom";
 import KickaLogo from "../../components/KickaLogo";
 
 const HomePage = () => {
-    const { data, isLoading } = useQuery("user", () => {
-        return axios.get<UserData>("private/user");
-    });
+    const query = useQueries([
+        {
+            queryKey: "user",
+            queryFn: () => {
+                const res = axios.get<UserData>("private/user");
+                return res;
+            },
+        },
+        {
+            queryKey: "games",
+            queryFn: async () => {
+                const res = axios.get("private/games");
+                return res;
+            },
+        },
+    ]);
 
     return (
         <div className="flex flex-col grow gap-5">
             <KickaLogo />
-            <h2>Welcome back! {data?.data.username}</h2>
+            <h2>Welcome back! {query[0].data?.data.username}</h2>
             <Link to="play">
                 <button className="button bg-primary-action">Play</button>
             </Link>
+            <div>
+                <h1>Games</h1>
+                <pre>{JSON.stringify(query[1].data?.data)}</pre>
+            </div>
         </div>
     );
 };
