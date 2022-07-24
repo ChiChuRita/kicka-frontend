6a76854d7f23db49a70562b666bc207fd21a1d5b
chichurita <ra.singh069@gmail.com>
2022-07-24 15:33:57 +0200

    removed some padding

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as SettingIcon } from "./assets/settings_icon.svg";
 const AppLayout: React.FC = () => {
     return (
         <div className="flex flex-col justify-between md:max-w-2xl container bg-black">
-            <div className="flex grow px-6 pt-16 pb-0 max-w-full">
+            <div className="flex grow px-6 pt-12 pb-0 max-w-full">
                 <Outlet />
             </div>
             <div className="container flex justify-center gap-5 bg-neutral-800 p-4 rounded-t-3xl">
